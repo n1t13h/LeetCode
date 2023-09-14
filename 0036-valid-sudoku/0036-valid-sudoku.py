40f4b048da517fb03aafbe215c4493f23f4720f0
Nitish Patel <nitishp74@gmail.com>
2023-09-14 22:33:07 +0530

    Time: 100 ms (74.27%), Space: 16.4 MB (48.29%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,30 +1,43 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        def is_valid_group(group):
-            seen = set()
-            for value in group:
-                if value != '.':
-                    if value in seen:
-                        return False
-                    seen.add(value)
-            return True
+        def getEmptyCount(row):
+            count = 0
+            for i in row:
+                if i == ".":
+                    count += 1
+            return count
+        def isRowValid(rowNumber):
+            row = board[rowNumber]
+            return len(set(row)) == 10 - getEmptyCount(row)
+        def isColumnValid(colNumber):
+            column = []
+            for x in board:
+                column.append(x[colNumber])
+            return len(set(column)) == 10 - getEmptyCount(column)
+        def isCellValid(cell):
+            return len(set(cell)) == 10 - getEmptyCount(cell)
 
-        def get_3x3_block(row, col):
+        def get3by3(colNo, rowNo):
             block = []
-            for i in range(row, row + 3):
-                for j in range(col, col + 3):
-                    block.append(board[i][j])
+            for i in range(3):
+                for j in range(3):
+                    block.append(board[3 * rowNo + i][3 * colNo + j])
             return block
 
-        for i in range(9):
-            if not is_valid_group(board[i]):  # Check rows
-                return False
-            if not is_valid_group([board[j][i] for j in range(9)]):  # Check columns
+
+        for i in range(0, len(board)):
+            # print(f"isRowValid:{i} - ", isRowValid(i))
+            if not isRowValid(i):
                 return False
 
-        for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                if not is_valid_group(get_3x3_block(i, j)):  # Check 3x3 blocks
+        for i in range(0, len(board)):
+            if not isColumnValid(i):
+                return False
+
+
+        for i in range(3):
+            for j in range(3):
+                cell = get3by3(i, j)
+                if not isCellValid(cell):
                     return False
-
         return True
