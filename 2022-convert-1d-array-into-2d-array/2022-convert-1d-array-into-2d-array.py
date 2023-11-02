79313fa48b2e58052d2b8e7801af5d9e5a222968
Nitish Patel <nitishp74@gmail.com>
2023-11-02 22:39:25 +0530

    Time: 843 ms (11.09%), Space: 40.4 MB (7.15%) - LeetHub

---
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
@@ -0,0 +1,9 @@
+import numpy as np
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if len(original)!=m*n:
+            return []
+        matrix=[]
+        for i in range(0,len(original),n):
+            matrix.append(original[i:i+n])
+        return matrix
