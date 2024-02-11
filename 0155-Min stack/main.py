c6c2c4459f8ca0eccc92d3fc7f6bdef7f0f80270
Nitish Patel <nitishp74@gmail.com>
2024-02-11 12:23:17 +0530

    Better Min Stack

--- 0155-Min stack/main.py
+++ 0155-Min stack/main.py
@@ -2,6 +2,7 @@ class MinStack(object):
 
     def __init__(self):
         self.stack = []
+        self.min_stack = []
 
     def push(self, val):
         """
@@ -9,28 +10,31 @@ class MinStack(object):
         :rtype: None
         """
         self.stack.append(val)
-        
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
 
     def pop(self):
         """
         :rtype: None
         """
-        self.stack.pop()
-        
+        if self.stack:
+            val = self.stack.pop()
+            if val == self.min_stack[-1]:
+                self.min_stack.pop()
 
     def top(self):
         """
         :rtype: int
         """
-        return self.stack[-1]
-        
+        if self.stack:
+            return self.stack[-1]
 
     def getMin(self):
         """
         :rtype: int
         """
-        return min(self.stack)
-
+        if self.min_stack:
+            return self.min_stack[-1]
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
